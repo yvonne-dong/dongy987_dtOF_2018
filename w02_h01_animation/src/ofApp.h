3b6285acb354694241a6af010d238857e8c244ab
yvonne-dong <dongy987@newschool.edu>
2018-09-11 19:26:02 -0400

    hw

--- w02_h01_animation/src/ofApp.h
+++ w02_h01_animation/src/ofApp.h
@@ -13,6 +13,7 @@ public:
     
     Animation ani[30];        // declare a Square object
     
+    
     void keyPressed(int key);
     void keyReleased(int key);
     void mouseMoved(int x, int y );
